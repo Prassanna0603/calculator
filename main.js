fad1ec5075a21b96b59dfd83f1338f324c5171d6
Prassanna0603 <prassanna@gmail.com>
2022-12-08 17:34:29 +0530

    second commit

--- main.js
+++ main.js
@@ -2,24 +2,24 @@
 
 let result = document.getElementById("outputscreen");
 
-function display(num){
+function display(num) {
     result.value = result.value + num;
 }
 
 
-function calculate(){
-    try{
-        result.value =eval(result.value)
+function calculate() {
+    try {
+        result.value = eval(result.value)
     }
-    catch(err){
+    catch (err) {
         alert("Invalid Input")
     }
 }
-function Clear(){
-    result.value =" "; 
+function Clear() {
+    result.value = " ";
 
 }
-function del(){
-    result.value =  result.value.slice(0,-1); 
+function del() {
+    result.value = result.value.slice(0, -1);
 
 }
